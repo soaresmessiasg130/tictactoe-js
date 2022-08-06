512106e2bf2bd9ff241f1970532cafcbf4cc4ef7
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-08-06 16:05:08 -0300

    ajustando estilo

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const player1 = "x";
-const player2 = "o";
+const player1 = "X";
+const player2 = "O";
 var playTime = player1;
 var gameOver = false;
 
@@ -17,11 +17,15 @@ function inicializarEspacos() {
       if (this.getElementsByTagName("img").length == 0) {
         if (playTime == player1) {
           this.innerHTML = "<img src='_imagens/x.png'>";
+
           this.setAttribute("jogada", player1);
+
           playTime = player2;
         } else {
           this.innerHTML = "<img src='_imagens/o.png'>";
+
           this.setAttribute("jogada", player2);
+
           playTime = player1;
         }
       }
@@ -35,20 +39,16 @@ function inicializarEspacos() {
 
 function atualizaMostrador() {
   if (gameOver) {
-    var divVez = document.getElementById("vez");
-
-    console.log(divVez);
-
-    divVez.innerHTML = "";
-
     return;
   }
 
   if (playTime == player1) {
-    var player = document.querySelectorAll("div#vez img")[0];
+    var player = document.querySelectorAll("header img")[0];
+
     player.setAttribute("src", "_imagens/x.png");
   } else {
-    var player = document.querySelectorAll("div#vez img")[0];
+    var player = document.querySelectorAll("header img")[0];
+
     player.setAttribute("src", "_imagens/o.png");
   }
 }
@@ -57,11 +57,9 @@ function verificarVencedor() {
   var a1 = document.getElementById("a1").getAttribute("jogada");
   var a2 = document.getElementById("a2").getAttribute("jogada");
   var a3 = document.getElementById("a3").getAttribute("jogada");
-
-  var b1 = document.getElementById("a1").getAttribute("jogada");
+  var b1 = document.getElementById("b1").getAttribute("jogada");
   var b2 = document.getElementById("b2").getAttribute("jogada");
   var b3 = document.getElementById("b3").getAttribute("jogada");
-
   var c1 = document.getElementById("c1").getAttribute("jogada");
   var c2 = document.getElementById("c2").getAttribute("jogada");
   var c3 = document.getElementById("c3").getAttribute("jogada");
@@ -88,13 +86,8 @@ function verificarVencedor() {
   }
 
   if (vencedor != "") {
-    alert("O ganhador foi: '" + vencedor + "'!");
+    alert("'" + vencedor + "' venceu!");
 
     gameOver = true;
   }
-}
-
-function restart() {
-  alert("csakcaslkcns");
-  location.reload();
 }
