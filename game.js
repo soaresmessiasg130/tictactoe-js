30b3ce015596626a55c125bf38a34e82779005db
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2022-08-06 13:02:50 -0300

    AJustando Imagem O do game

--- game.js
+++ game.js
@@ -1,57 +1,57 @@
-const player1 = "X";
-const player2 = "O";
+const player1 = "x";
+const player2 = "o";
 var playTime = player1;
-var gameOver= false;
+var gameOver = false;
 
-atualizaMostrador();
 inicializarEspacos();
 
-function atualizaMostrador(){
-  if(gameOver){return;}
-
-  if(playTime == player1){
-    var player = document.querySelectorAll("div#vez img")[0];
-    player.setAttribute("src", "_imagens/x.png");
-
-  }else{
-    var player = document.querySelectorAll("div#vez img")[0];
-    player.setAttribute("src", "_imagens/o.png");
-  }
-}
-
-function inicializarEspacos(){
-
+function inicializarEspacos() {
   var espacos = document.getElementsByClassName("espaco");
 
   for (var i = 0; i < espacos.length; i++) {
+    espacos[i].addEventListener("click", function () {
+      if (gameOver) {
+        return;
+      }
 
-    espacos[i].addEventListener("click", function(){
-
-        if (gameOver) {return;}
-
-        if(this.getElementsByTagName("img").length == 0){
-
-          if(playTime == player1){
-
-            this.innerHTML = "<img src='_imagens/X.png'>";
-            this.setAttribute("jogada", player1);
-            playTime = player2;
-
-          }else{
-
-            this.innerHTML = "<img src='_imagens/O.png'>";
-            this.setAttribute("jogada", player2);
-            playTime = player1;
-
-          }
-          atualizaMostrador();
-          verificarVencedor();
-
+      if (this.getElementsByTagName("img").length == 0) {
+        if (playTime == player1) {
+          this.innerHTML = "<img src='_imagens/x.png'>";
+          this.setAttribute("jogada", player1);
+          playTime = player2;
+        } else {
+          this.innerHTML = "<img src='_imagens/o.png'>";
+          this.setAttribute("jogada", player2);
+          playTime = player1;
         }
+      }
+
+      verificarVencedor();
+
+      atualizaMostrador();
     });
   }
 }
 
+function atualizaMostrador() {
+  if (gameOver) {
+    var divVez = document.getElementById("vez");
+
+    console.log(divVez);
+
+    divVez.innerHTML = "";
+
+    return;
+  }
+
+  if (playTime == player1) {
+    var player = document.querySelectorAll("div#vez img")[0];
+    player.setAttribute("src", "_imagens/x.png");
+  } else {
+    var player = document.querySelectorAll("div#vez img")[0];
+    player.setAttribute("src", "_imagens/o.png");
+  }
+}
 
 function verificarVencedor() {
   var a1 = document.getElementById("a1").getAttribute("jogada");
@@ -68,20 +68,33 @@ function verificarVencedor() {
 
   var vencedor = "";
 
-  if(   (a1 == b1 && a1 == c1 && a1 != "")
-    ||  (a1 == a2 && a1 == a3 && a1 != "")
-    ||  (a1 == b2 && a1 == c3 && a1 != "")  ){
+  if (
+    (a1 == b1 && a1 == c1 && a1 != "") ||
+    (a1 == a2 && a1 == a3 && a1 != "") ||
+    (a1 == b2 && a1 == c3 && a1 != "")
+  ) {
     vencedor = a1;
-  }else if(   (b2 == b1 && b2 == b3 && b2 != "")
-           || (b2 == a2 && b2 == c2 && b2 != "")
-           || (b2 == a3 && b2 == c1 && b2 != "")  ){
+  } else if (
+    (b2 == b1 && b2 == b3 && b2 != "") ||
+    (b2 == a2 && b2 == c2 && b2 != "") ||
+    (b2 == a3 && b2 == c1 && b2 != "")
+  ) {
     vencedor = b2;
-  }else if(   (c3 == c2 && c3 == c1 && c3 != "")
-          ||  (c3 == b3 && c3 == a3 && c3 != "")  ){
+  } else if (
+    (c3 == c2 && c3 == c1 && c3 != "") ||
+    (c3 == b3 && c3 == a3 && c3 != "")
+  ) {
     vencedor = c3;
   }
-  if(vencedor != ""){
-    gameOver = true;
+
+  if (vencedor != "") {
     alert("O ganhador foi: '" + vencedor + "'!");
+
+    gameOver = true;
   }
+}
+
+function restart() {
+  alert("csakcaslkcns");
+  location.reload();
 }
